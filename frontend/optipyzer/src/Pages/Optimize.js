41e03741b4f255cb86657b8d3c5bf4eed136e853
Nathan LeRoy <NLeRoy917@gmail.com>
2020-04-10 09:19:22 -0400

    prep for heroku deployment

--- frontend/optipyzer/src/Pages/Optimize.js
+++ frontend/optipyzer/src/Pages/Optimize.js
@@ -27,7 +27,7 @@ import ColorPalette from '../Resources/ColorPalette'
 // import axios
 const axios = require('axios').default;
 
-const API_URL = 'http://127.0.0.1:5000/'
+const API_URL = process.env.REACT_APP_API_URL
 
 // Or Create your Own theme:
 const theme = createMuiTheme({
--- frontend/optipyzer/src/Pages/Results.js
+++ frontend/optipyzer/src/Pages/Results.js
@@ -19,7 +19,7 @@ import ColorPalette from '../Resources/ColorPalette'
 // import axios
 const axios = require('axios').default;
 
-const API_URL = 'http://127.0.0.1:5000/'
+const API_URL = process.env.REACT_APP_API_URL
 
 const useStyles = makeStyles(({ breakpoints, spacing }) => ({
   formPaper: {
