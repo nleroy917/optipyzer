d8b3eb8854f12b0e43163cd1c0d87eaa1fa00d4f
Nathan LeRoy <NLeRoy917@gmail.com>
2020-04-16 17:14:26 -0400

    simplify travis-ci test

--- sdk/optipyzer/api.py
+++ sdk/optipyzer/api.py
@@ -166,19 +166,6 @@ if __name__ == '__main__':
 
 	optipyzer = api()
 
-	results = optipyzer.search(name='Escherichia Coli')
-	org1 = results[0]
-
-	results = optipyzer.search(name='Campylobacter')
-	org2 = results[0]
-
-	codon_usage1 = optipyzer.pull_codons(org1)
-	codon_usage2 = optipyzer.pull_codons(org2)
-
-	seq = 'ATGGCTACTGCATGCTTAGCATGCATGACT'
-	optimized = optipyzer.optimize(seq,org_list=[org1,org2],weights=[1,2])
-
-	print(optimized.optimmized_sd)
 
 
 
