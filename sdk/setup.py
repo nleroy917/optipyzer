fe75999d9e27a67fb7011c419177f54cf21d1aaa
Nathan LeRoy <NLeRoy917@gmail.com>
2020-04-17 11:18:36 -0400

    add header img

---
+++ sdk/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+# create long desc.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# get requirements
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='optipyzer',
+    version=0.1,
+    description='Codon optimize DNA and Protein sequences for multiple species.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="Apache 2.0",
+    author='Nathan LeRoy',
+    author_email='NLeRoy917@gmail.com',
+    url='https://github.com/NLeRoy917/optipyzer',
+    keywords='codon optimization dna protein biology bioinformatics',
+    packages=setuptools.find_packages()
+    install_requires=requirements,
+    classifiers=[
+        'Topic :: Software Development :: Libraries',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: Apache License, Version 2.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
