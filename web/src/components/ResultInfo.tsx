b4494771dcb48facf33be33d7b74dceae75c79ec
Nathan LeRoy <NLeRoy917@gmail.com>
2021-10-29 21:20:30 -0400

    update result info

--- web/src/components/ResultInfo.tsx
+++ web/src/components/ResultInfo.tsx
@@ -17,7 +17,7 @@ const ResultInfo: FC<Props> = (props) => {
             className="w-10/12 p-2 mt-4 text-sm bg-yellow-400 border-2 border-black rounded-lg shadow-2xl md:w-1/2 -top-0 md:text-base md:top-0"
           >
             <div className="flex flex-row justify-between mx-1">
-              <p className="text-lg font-bold">About query results:</p>
+              <p className="text-xl font-bold">About query results:</p>
               <button
                 className="hover:cursor-pointer hover:text-gray-700"
                 onClick={() => setOpen(false)}
@@ -26,7 +26,12 @@ const ResultInfo: FC<Props> = (props) => {
               </button>
             </div>
             <div className="p-1 font-normal">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit eum quae ratione ducimus sed animi fugit obcaecati, culpa nulla tenetur. Quas, dolorum recusandae? Esse laborum explicabo obcaecati, enim delectus magnam.
+              Your optimized query will consider the codon usage bias of one or more organisms. Below you are given two optimized sequences:
+              <ul className="my-2">
+                <li><span className="ml-2 font-bold">Optimized AD:</span> Your opimized sequence using the</li>
+                <li><span className="ml-2 font-bold">Optimized SD:</span> Your opimized sequence using the</li>
+              </ul>
+              As well, you are given the translated sequence.
             </div>
           </div>
         </div>
