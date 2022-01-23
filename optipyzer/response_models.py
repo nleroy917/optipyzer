591b0cdd6d7d46023ea5b1e196c072ed0e12bb7a
Nathan LeRoy <NLeRoy917@gmail.com>
2022-01-23 08:38:01 -0500

    fix data typing issues

--- optipyzer/response_models.py
+++ optipyzer/response_models.py
@@ -2,20 +2,22 @@ from typing import List
 from pydantic import BaseModel
 
 class Organism(BaseModel):
-    GC1_perc: int
-    GC2_perc: int
-    GC3_perc: int
-    GC_perc: int
-    assembly: str
+    org_id: int
     division: str
+    assembly: str
+    taxid: int
+    species: str
+    organelle: str
+    translation_table: int
     num_CDS: int
     num_codons: int
-    org_id: int
-    organelle: str
-    species: str
-    taxid: int
-    translation_table: int
+    GC_perc: float
+    GC1_perc: float
+    GC2_perc: float
+    GC3_perc: float
 
+    class Config:
+        orm_mode = True
 
 class SearchResult(BaseModel):
     num_results: int
