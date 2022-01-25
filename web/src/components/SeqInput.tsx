277089189fc558cc85cd745dc1864e9ac9078465
Nathan LeRoy <NLeRoy917@gmail.com>
2022-01-24 20:09:44 -0500

    frontend sequence validation

--- web/src/components/SeqInput.tsx
+++ web/src/components/SeqInput.tsx
@@ -1,26 +1,56 @@
-import { Dispatch, FC, SetStateAction } from "react";
+import { Dispatch, FC, SetStateAction, useState } from "react";
+
+import { validateSequence} from "@/utils/validateSequence";
 
 interface Props {
   seq: string;
+  seqType: string;
   setSeq: Dispatch<SetStateAction<string>>;
 }
 
 const SeqInput: FC<Props> = (props) => {
   
   // destructure props
-const { seq, setSeq } = props
+  const { seq, setSeq, seqType } = props
+
+  // invalid seq flag
+  const [ seqIsInvalid, setSeqIsInvalid ] = useState<boolean>(false);
+
+  // message
+  const [seqValidationMessage, setSeqValidationMessage] = useState<null | string>(null)
+
+  // styling
+  const validStyling = "w-full p-2 mb-2 text-sm text-gray-600 border border-black rounded-lg shadow-md md:w-3/4"
+  const invalidStyling = "w-full p-2 mb-2 text-sm text-red-600 border border-red-600 rounded-lg shadow-md md:w-3/4 focus:border-red-600"
 
   return (
-    <textarea
-        value={seq}
-        onChange={e => {
-          let seq = e.target.value
-          seq = seq.replace((/ {2}|\r\n|\n|\r/gm),"");
-          setSeq(seq)
-        }}
-        className="w-full p-2 mb-2 text-sm text-gray-600 border border-black rounded-lg shadow-md md:w-3/4"
-        rows={15}
-    />
+    <div className="w-full">
+      <textarea
+          value={seq}
+          onChange={e => {
+
+            let s = e.target.value
+            s = s.replace((/ {2}|\r\n|\n|\r/gm),"");
+
+            const { isValid, message } = validateSequence(s, seqType)
+
+            if (!isValid) {
+              setSeqIsInvalid(true)
+              setSeqValidationMessage(message)
+            } else {
+              setSeqIsInvalid(false)
+              setSeqValidationMessage(null)
+            }
+            
+            setSeq(s)
+          }}
+          className={seqIsInvalid ? invalidStyling : validStyling}
+          rows={15}
+      />
+      <div className="absolute text-red-600 translate-x-4 -translate-y-12">
+        {seqValidationMessage || ' '}
+      </div>
+    </div>
   )
 }
 
