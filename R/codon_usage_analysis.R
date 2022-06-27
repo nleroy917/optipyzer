491146f564cc78b4927d6520f1bb646725ce3145
Nathan LeRoy <NLeRoy917@gmail.com>
2022-06-27 12:01:36 -0400

    update analysis

--- R/codon_usage_analysis.R
+++ R/codon_usage_analysis.R
@@ -32,11 +32,33 @@ amino.acid.counts <- aggregate(codon.counts.df$count_billion, by=list(amino_acid
 names(amino.acid.counts)[names(amino.acid.counts) == 'x'] <- 'count_billion' # rename
 
 # plot codon counts
-p <- ggplot(amino.acid.counts, aes(x=reorder(amino_acid, -count_billion), y=count_billion)) +
+pAA <- ggplot(amino.acid.counts, aes(x=reorder(amino_acid, -count_billion), y=count_billion)) +
   geom_bar(stat="identity", fill="black") + 
   theme_classic() + 
   theme(
     axis.text.x = element_text(size=10)
   ) +
+  scale_y_continuous(limits=c(0,18), expand = c(0, 0)) +
   xlab("Residue") + ylab("Count (Billion)")
-p
+
+pCodon <- ggplot(codon.counts.df, aes(x=reorder(codon, -count_billion), y=count_billion)) +
+  geom_bar(stat="identity", fill="black") + 
+  theme_classic() + 
+  theme(
+    axis.text.x = element_text(
+      size=10, angle = 90, vjust = 0.5, hjust=1
+    ),
+    axis.text.y = element_text(
+      size=14
+    ),
+    axis.title.x = element_text(
+      size=16
+    ),
+    axis.title.y = element_text(
+      size=16
+    )
+  ) +
+  scale_y_continuous(limits=c(0,6.5), expand = c(0, 0)) +
+  xlab("Codon") + ylab("Count (Billion)")
+pCodon
+ggsave("codon_counts.tiff", units="px", width = 3200 , height = 2400, device='tiff', dpi=300)
