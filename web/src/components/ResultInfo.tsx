488542e1deaa2fd20bd29ac972bcc9e3fa8af1db
Nathan LeRoy <NLeRoy917@gmail.com>
2023-09-12 11:14:59 -0400

    complete the about-query-results modal (#56)

--- web/src/components/ResultInfo.tsx
+++ web/src/components/ResultInfo.tsx
@@ -16,9 +16,9 @@ const ResultInfo: FC<Props> = (props) => {
         className="w-10/12 p-2 mt-4 text-sm bg-yellow-400 border-2 border-black rounded-lg shadow-2xl md:w-1/2 -top-0 md:text-base md:top-0"
       >
         <div className="flex flex-row justify-between mx-1">
-          <p className="text-xl font-bold">About query results:</p>
+          <p className="text-2xl font-bold">About query results:</p>
           <button
-            className="hover:cursor-pointer hover:text-gray-700"
+            className="text-2xl hover:cursor-pointer hover:text-gray-700"
             onClick={() => setOpen(false)}
           >
             ⓧ
@@ -27,17 +27,20 @@ const ResultInfo: FC<Props> = (props) => {
         <div className="p-1 font-normal">
           Your optimized query will consider the codon usage bias of one or more
           organisms. Below you are given two optimized sequences:
-          <ul className="my-2">
+          <ul className="my-2 list-decimal list-inside">
             <li>
               <span className="ml-2 font-bold">Optimized AD:</span> Your
-              opimized sequence using the
+              opimized sequence using the <b>absolute difference</b> difference between the predicted and actual codon frequencies.
             </li>
             <li>
               <span className="ml-2 font-bold">Optimized SD:</span> Your
-              opimized sequence using the
+              opimized sequence using the <b>squared error</b> difference between the predicted and actual codon frequencies.
             </li>
           </ul>
-          As well, you are given the translated sequence.
+
+          If you are unsure which sequence to use, <i>we recommend using the optimized SD sequence</i>. See {' '}
+          <a className='font-bold text-blue-600 hover:underline' href="https://github.com/nleroy917/optipyzer/issues/56">this issue</a> for more information.
+          We also give you the translated amino acid sequence of your optimized nucleotide sequence.
         </div>
       </div>
     </div>
